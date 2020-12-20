dc269d5d5ff1da17ac90636f2d9beca23cc9aa70
Khuong Tran <dbcgdhh@gmail.com>
2020-12-20 20:56:30 +0700

    Style sidebar

--- src/components/SideBar.vue
+++ src/components/SideBar.vue
@@ -1,18 +1,71 @@
 <template>
-  <div data-cy="side-bar">
-    <router-link to="/bao-hiem/o-to">Ô tô</router-link>
-    <router-link to="/bao-hiem/tai-nan">Tai nạn 24/24</router-link>
-    <router-link to="/bao-hiem/suc-khoe-ca-nhan">Sức khỏe cá nhân</router-link>
-    <router-link to="/bao-hiem/ung-thu">Ung thư</router-link>
-    <router-link to="/bao-hiem/nha-tu-nhan">Nhà tư nhân</router-link>
-    <router-link to="/bao-hiem/xe-may">Xe máy</router-link>
-    <router-link to="/bao-hiem/du-lich-quoc-te">Du lịch quốc tế</router-link>
-    <router-link to="/bao-hiem/nhan-tho">Nhân thọ</router-link>
-  </div>
+  <v-list data-cy="side-bar" nav>
+    <v-subheader>YÊU CẦU MUA BẢO HIỂM</v-subheader>
+
+    <v-list-item-group v-model="selectedItem" color="primary">
+      <v-list-item v-for="({ icon, text, to }, i) in items" :key="i" :to="to">
+        <v-list-item-icon>
+          <v-icon>{{ icon }}</v-icon>
+        </v-list-item-icon>
+
+        <v-list-item-content>
+          <v-list-item-title>{{ text }}</v-list-item-title>
+        </v-list-item-content>
+      </v-list-item>
+    </v-list-item-group>
+  </v-list>
 </template>
 
 <script>
 export default {
-  name: "SideBar"
+  name: "SideBar",
+
+  data() {
+    return {
+      selectedItem: 0,
+      items: [
+        {
+          icon: "mdi-car",
+          text: "Ô tô",
+          to: "/bao-hiem/o-to"
+        },
+        {
+          icon: "mdi-car-cog",
+          text: "Tai nạn 24/24",
+          to: "/bao-hiem/tai-nan"
+        },
+        {
+          icon: "mdi-emoticon-sick",
+          text: "Sức khỏe cá nhân",
+          to: "/bao-hiem/suc-khoe-ca-nhan"
+        },
+        {
+          icon: "mdi-zodiac-cancer",
+          text: "Ung thư",
+          to: "/bao-hiem/ung-thu"
+        },
+        {
+          icon: "mdi-home",
+          text: "Nhà tư nhân",
+          to: "/bao-hiem/nha-tu-nhan"
+        },
+        {
+          icon: "mdi-motorbike",
+          text: "Xe máy",
+          to: "/bao-hiem/xe-may"
+        },
+        {
+          icon: "mdi-airplane-takeoff",
+          text: "Du lịch quốc tế",
+          to: "/bao-hiem/du-lich-quoc-te"
+        },
+        {
+          icon: "mdi-human-male",
+          text: "Nhân thọ",
+          to: "/bao-hiem/nhan-tho"
+        }
+      ]
+    };
+  }
 };
 </script>
