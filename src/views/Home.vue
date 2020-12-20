aac9472d079798aea2c8b12de4e6475286f661f0
Khuong Tran <dbcgdhh@gmail.com>
2020-12-20 13:50:27 +0700

    Add tests for link to insurance requests

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,41 @@
 <template>
   <div class="home">
     Khách hàng đăng ký mua bảo hiểm
+    <router-link data-cy="car-insurance-request" to="/bao-hiem/o-to"
+      >Bảo hiểm ô tô</router-link
+    >
+
+    <router-link data-cy="accident-insurance-request" to="/bao-hiem/tai-nan"
+      >Bảo hiểm tai nạn</router-link
+    >
+
+    <router-link
+      data-cy="health-insurance-request"
+      to="/bao-hiem/suc-khoe-ca-nhan"
+      >Bảo hiểm sức khỏe cá nhân</router-link
+    >
+
+    <router-link data-cy="cancer-insurance-request" to="/bao-hiem/ung-thu"
+      >Bảo hiểm ung thư</router-link
+    >
+
+    <router-link data-cy="house-insurance-request" to="/bao-hiem/nha-tu-nhan"
+      >Bảo hiểm nhà tư nnân</router-link
+    >
+
+    <router-link data-cy="motorbike-insurance-request" to="/bao-hiem/xe-may"
+      >Bảo hiểm xe máy</router-link
+    >
+
+    <router-link
+      data-cy="travel-insurance-request"
+      to="/bao-hiem/du-lich-quoc-te"
+      >Bảo hiểm du lịch quốc tế</router-link
+    >
+
+    <router-link data-cy="life-insurance-request" to="/bao-hiem/nhan-tho"
+      >Bảo hiểm nhân thọ</router-link
+    >
   </div>
 </template>
 
--- tests/e2e/specs/home.spec.js
+++ tests/e2e/specs/home.spec.js
@@ -1,6 +1,65 @@
 describe("Home page", () => {
-  it("Render text 'Khách hàng đăng ký mua bảo hiểm'", () => {
+  beforeEach(() => {
     cy.visit("/");
+  });
+
+  it("Renders text 'Khách hàng đăng ký mua bảo hiểm'", () => {
     cy.contains("Khách hàng đăng ký mua bảo hiểm").should("be.visible");
+  });
+
+  it("Render button linking to car insurance buying requests", () => {
+    const carInsuranceSelector = "[data-cy=car-insurance-request]";
+    cy.get(carInsuranceSelector).should("be.visible");
+    cy.get(carInsuranceSelector).click();
+    cy.url().should("include", "/bao-hiem/o-to");
+  });
+
+  it("Render button linking to accident insurance buying requests", () => {
+    const carInsuranceSelector = "[data-cy=accident-insurance-request]";
+    cy.get(carInsuranceSelector).should("be.visible");
+    cy.get(carInsuranceSelector).click();
+    cy.url().should("include", "/bao-hiem/tai-nan");
+  });
+
+  it("Render button linking to health insurance buying requests", () => {
+    const carInsuranceSelector = "[data-cy=health-insurance-request]";
+    cy.get(carInsuranceSelector).should("be.visible");
+    cy.get(carInsuranceSelector).click();
+    cy.url().should("include", "/bao-hiem/suc-khoe-ca-nhan");
+  });
+
+  it("Render button linking to cancer insurance buying requests", () => {
+    const carInsuranceSelector = "[data-cy=cancer-insurance-request]";
+    cy.get(carInsuranceSelector).should("be.visible");
+    cy.get(carInsuranceSelector).click();
+    cy.url().should("include", "/bao-hiem/ung-thu");
+  });
+
+  it("Render button linking to house insurance buying requests", () => {
+    const carInsuranceSelector = "[data-cy=house-insurance-request]";
+    cy.get(carInsuranceSelector).should("be.visible");
+    cy.get(carInsuranceSelector).click();
+    cy.url().should("include", "/bao-hiem/nha-tu-nhan");
+  });
+
+  it("Render button linking to motorbike insurance buying requests", () => {
+    const carInsuranceSelector = "[data-cy=motorbike-insurance-request]";
+    cy.get(carInsuranceSelector).should("be.visible");
+    cy.get(carInsuranceSelector).click();
+    cy.url().should("include", "/bao-hiem/xe-may");
+  });
+
+  it("Render button linking to travel insurance buying requests", () => {
+    const carInsuranceSelector = "[data-cy=travel-insurance-request]";
+    cy.get(carInsuranceSelector).should("be.visible");
+    cy.get(carInsuranceSelector).click();
+    cy.url().should("include", "/bao-hiem/du-lich-quoc-te");
+  });
+
+  it("Render button linking to life insurance buying requests", () => {
+    const carInsuranceSelector = "[data-cy=life-insurance-request]";
+    cy.get(carInsuranceSelector).should("be.visible");
+    cy.get(carInsuranceSelector).click();
+    cy.url().should("include", "/bao-hiem/nhan-tho");
   });
 });
