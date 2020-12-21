e8d7ca55b3caf344feed291fe8b9c4c32c7b09d4
Khuong Tran <dbcgdhh@gmail.com>
2020-12-21 18:58:52 +0700

    Color buttons

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -9,12 +9,13 @@
     <v-row>
       <v-col v-for="(nav, index) in navs" :key="index">
         <v-btn
-          :data-cy="nav.dataCy"
-          :to="nav.to"
-          color="blue-grey"
-          class="ma-2 white--text"
+          :color="getButtonBackgroundColor(index)"
+          class="ma-2"
+          :class="getButtonTextColor(index) + '--text'"
           x-large
           fab
+          :data-cy="nav.dataCy"
+          :to="nav.to"
         >
           <v-icon dark>{{ nav.icon }}</v-icon>
         </v-btn>
@@ -29,8 +30,53 @@ import InsuranceNavs, { Nav } from "@/utils/insurance-navs";
 
 const insuranceNavsInstance = new InsuranceNavs();
 
+enum BUTTON_POSITION {
+  CAR,
+  ACCIDENT,
+  HEALTH,
+  CANCER,
+  HOUSE,
+  MOTORBIKE,
+  TRAVEL,
+  LIFE
+}
+
 @Component
 export default class Home extends Vue {
   navs: Nav[] = insuranceNavsInstance.navs;
+
+  getButtonBackgroundColor(buttonIndex: number): string {
+    switch (buttonIndex) {
+      case BUTTON_POSITION.CAR:
+        return "indigo";
+      case BUTTON_POSITION.ACCIDENT:
+        return "red";
+      case BUTTON_POSITION.HEALTH:
+        return "grey lighten-1";
+      case BUTTON_POSITION.CANCER:
+        return "warning";
+      case BUTTON_POSITION.HOUSE:
+        return "success";
+      case BUTTON_POSITION.MOTORBIKE:
+        return "brown darken-1";
+      case BUTTON_POSITION.TRAVEL:
+        return "info";
+      case BUTTON_POSITION.LIFE:
+        return "black";
+      default:
+        return "";
+    }
+  }
+
+  getButtonTextColor(buttonIndex: number): string {
+    switch (buttonIndex) {
+      case BUTTON_POSITION.HEALTH:
+        return "black";
+      case BUTTON_POSITION.LIFE:
+        return "yellow";
+      default:
+        return "white";
+    }
+  }
 }
 </script>
