5aecd81393531069c3cdeef46ec3ab505bd9b176
Khuong Tran <dbcgdhh@gmail.com>
2020-12-21 18:45:10 +0700

    Positioni buttons

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,14 +3,17 @@
     <v-row>
       <v-col>
         <h2>Khách hàng đăng ký mua bảo hiểm</h2>
+      </v-col>
+    </v-row>
 
+    <v-row>
+      <v-col v-for="(nav, index) in navs" :key="index">
         <v-btn
-          v-for="(nav, index) in navs"
-          :key="index"
           :data-cy="nav.dataCy"
           :to="nav.to"
           color="blue-grey"
           class="ma-2 white--text"
+          x-large
           fab
         >
           <v-icon dark>{{ nav.icon }}</v-icon>
