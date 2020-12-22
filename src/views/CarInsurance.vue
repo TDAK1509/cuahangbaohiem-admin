c9c72ff4b1a3a6ea402ee14b3014ecf62dbb0055
Khuong Tran <dbcgdhh@gmail.com>
2020-12-23 06:55:07 +0700

    Pass test render cells

--- src/views/CarInsurance.vue
+++ src/views/CarInsurance.vue
@@ -13,6 +13,13 @@
             </th>
           </tr>
         </thead>
+        <tbody>
+          <tr v-for="(row, index) in tableBody" :key="`row${index}`">
+            <td v-for="(cell, cellIndex) in row" :key="`cell${cellIndex}`">
+              {{ cell }}
+            </td>
+          </tr>
+        </tbody>
       </template>
     </v-simple-table>
   </v-container>
@@ -31,6 +38,18 @@ export default class CarInsurance extends Vue {
     "Công ty bảo hiểm",
     "Số tiền bảo hiểm",
     "Ghi chú"
+  ];
+
+  tableBody = [
+    {
+      date: "2020-12-23 06:43:56",
+      name: "Michael Jackson",
+      email: "test@gmail.com",
+      phone: "1234567",
+      insuranceCompany: "PVI",
+      insuranceValue: "12.320.000 ₫",
+      note: ""
+    }
   ];
 }
 </script>
--- tests/e2e/specs/car-insurance.spec.js
+++ tests/e2e/specs/car-insurance.spec.js
@@ -3,7 +3,7 @@ describe("bao-hiem/o-to page", () => {
     cy.visit("/bao-hiem/o-to");
   });
 
-  it("renders table with correct titles", () => {
+  it("renders table with correct headings", () => {
     cy.contains("Ngày").should("be.visible");
     cy.contains("Tên").should("be.visible");
     cy.contains("Email").should("be.visible");
@@ -11,5 +11,29 @@ describe("bao-hiem/o-to page", () => {
     cy.contains("Công ty bảo hiểm").should("be.visible");
     cy.contains("Số tiền bảo hiểm").should("be.visible");
     cy.contains("Ghi chú").should("be.visible");
+  });
+
+  it("renders date", () => {
+    cy.contains("2020-12-23 06:43:56").should("be.visible");
+  });
+
+  it("renders name", () => {
+    cy.contains("Michael Jackson").should("be.visible");
+  });
+
+  it("renders email", () => {
+    cy.contains("test@gmail.com").should("be.visible");
+  });
+
+  it("renders phone", () => {
+    cy.contains("1234567").should("be.visible");
+  });
+
+  it("renders insurance company", () => {
+    cy.contains("PVI").should("be.visible");
+  });
+
+  it("renders insurance value", () => {
+    cy.contains("12.320.000 ₫").should("be.visible");
   });
 });
