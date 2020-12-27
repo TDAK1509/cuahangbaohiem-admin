34ce3ef142e898b52d3b6e24538fcf6cb7b4dd67
Khuong Tran <dbcgdhh@gmail.com>
2020-12-27 09:17:24 +0700

    Remove unecessary firebase info

--- src/models/fire.ts
+++ src/models/fire.ts
@@ -21,12 +21,7 @@ export default class FireBase {
     const firebaseConfig = {
       apiKey: process.env.VUE_APP_FIREBASE_API_KEY,
       authDomain: process.env.VUE_APP_FIREBASE_AUTH_DOMAIN,
-      databaseURL: process.env.VUE_APP_FIREBASE_DATABASE_URL,
-      projectId: process.env.VUE_APP_FIREBASE_PROJECT_ID,
-      storageBucket: process.env.VUE_APP_FIREBASE_STORAGE_BUCKET,
-      messagingSenderId: process.env.VUE_APP_FIREBASE_MESSAGING_SENDER_ID,
-      appId: process.env.VUE_APP_FIREBASE_APP_ID,
-      measurementId: process.env.VUE_APP_FIREBASE_MEASUREMENT_ID
+      projectId: process.env.VUE_APP_FIREBASE_PROJECT_ID
     };
 
     if (firebase.apps.length === 0) {
