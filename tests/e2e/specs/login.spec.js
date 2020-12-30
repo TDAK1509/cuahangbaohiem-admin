d6db3f57941ebd39d317f9a2af4c48aaae843c0b
Khuong Tran <dbcgdhh@gmail.com>
2020-12-30 21:05:31 +0700

    Test wrong email format

--- tests/e2e/specs/login.spec.js
+++ tests/e2e/specs/login.spec.js
@@ -4,23 +4,35 @@ describe("Login page", () => {
     cy.url().should("include", "/login");
   });
 
-  describe("rendering", () => {
-    it("does not render side bar", () => {
-      cy.get("[data-cy=side-bar]").should("not.exist");
+  describe("login page", () => {
+    beforeEach(() => {
+      cy.visit("/login");
     });
 
-    it("renders email field, password field, and login button", () => {
-      getEmailField().should("be.visible");
-      getPasswordField().should("be.visible");
-      getLoginButton().should("be.visible");
-    });
-  });
+    describe("rendering", () => {
+      it("does not render side bar", () => {
+        cy.get("[data-cy=side-bar]").should("not.exist");
+      });
 
-  describe("form handling", () => {
-    it("clicks LOGIN button shows 2 errors", () => {
-      getLoginButton().click();
-      cy.contains("Vui lòng điền email").should("be.visible");
-      cy.contains("Vui lòng điền mật khẩu").should("be.visible");
+      it("renders email field, password field, and login button", () => {
+        getEmailField().should("be.visible");
+        getPasswordField().should("be.visible");
+        getLoginButton().should("be.visible");
+      });
+    });
+
+    describe("form handling", () => {
+      it("clicks LOGIN button shows 2 errors", () => {
+        getLoginButton().click();
+        cy.contains("Vui lòng điền email").should("be.visible");
+        cy.contains("Vui lòng điền mật khẩu").should("be.visible");
+      });
+
+      it("enter wrongs email shows correct message", () => {
+        getEmailField().type("wrong-email");
+        getLoginButton().click();
+        cy.contains("Định dạng email không chính xác").should("be.visible");
+      });
     });
   });
 });
