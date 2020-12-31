3e5356f8888445c067d09aa885b102563c9f2de9
Khuong Tran <dbcgdhh@gmail.com>
2020-12-31 11:33:14 +0700

    Script to create firebase user in emulator

---
+++ generate-dummy-data/user.js
@@ -0,0 +1,41 @@
+require("dotenv").config();
+
+const admin = require("firebase-admin");
+
+const FIRESTORE_URL = "localhost:8081";
+const AUTH_URL = "localhost:9099";
+const PROJECT_ID = process.env.VUE_APP_FIREBASE_PROJECT_ID;
+
+(async () => {
+  await initializeEmulatorAdmin("localhost")
+    .auth()
+    .createUser({
+      uid: "ID",
+      email: "correct-email@gmail.com",
+      password: "correct-password"
+    });
+
+  console.log("User created successfully");
+  process.exit();
+})();
+
+function initializeEmulatorAdmin(name, credential = null) {
+  initializeEmulatorVariables();
+
+  const options = {
+    projectId: PROJECT_ID
+  };
+
+  if (credential) {
+    options.credential = credential;
+  }
+
+  return admin.apps.length
+    ? admin.app(name)
+    : admin.initializeApp(options, name);
+}
+
+function initializeEmulatorVariables() {
+  process.env.FIRESTORE_EMULATOR_HOST = FIRESTORE_URL;
+  process.env.FIREBASE_AUTH_EMULATOR_HOST = AUTH_URL;
+}
