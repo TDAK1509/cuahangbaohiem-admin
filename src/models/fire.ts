7c5b7848153bec648fa4344ff9ac246a33164ae0
Tran Duy An Khuong <dbcgdhh@gmail.com>
2021-01-01 09:16:57 +0700

    Add e2e workflow (#7)
    * Add e2e workflow
    
    * Update fire model
    
    * test
    
    * Fix .env file
    
    * Add firebase token

--- src/models/fire.ts
+++ src/models/fire.ts
@@ -28,7 +28,9 @@ export default class FireBase {
     const firebaseConfig = {
       apiKey: process.env.VUE_APP_FIREBASE_API_KEY,
       authDomain: process.env.VUE_APP_FIREBASE_AUTH_DOMAIN,
-      projectId: process.env.VUE_APP_FIREBASE_PROJECT_ID
+      projectId: process.env.VUE_APP_FIREBASE_PROJECT_ID,
+      messagingSenderId: process.env.VUE_APP_FIREBASE_MESSAGING_SENDER_ID,
+      appId: process.env.VUE_APP_FIREBASE_APP_ID
     };
 
     if (firebase.apps.length === 0) {
